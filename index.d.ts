54294c33ae82e49b1cd72ae1328a5839ce3cd787
Håkan Arvidsson <harvids@gmail.com>
2024-02-12 10:00:08 +0100

    ttninjs16

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ export { TTVariantOfIPTCNinjsNewsInJSONVersion11 as TTNinjs11 } from './ttninjs_
 export { TTVariantOfIPTCNinjsNewsInJSONVersion13 as TTNinjs13 } from './ttninjs_13'
 export { TTVariantOfIPTCNinjsNewsInJSONVersion14 as TTNinjs14 } from './ttninjs_14'
 export { TTVariantOfIPTCNinjsNewsInJSONVersion15 as TTNinjs15 } from './ttninjs_15'
+export { TTVariantOfIPTCNinjsNewsInJSONVersion16 as TTNinjs16 } from './ttninjs_16'
 
 export const enum Variant {
     Normal = "Normal",
